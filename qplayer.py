98447a3d266da2f4ebd5ed6ca058b67ba4729309
Robert M Ochshorn <mail@RMOZONE.COM>
2012-10-09 14:28:21 +0200

    QT/Numm-based player

---
+++ qplayer.py
@@ -0,0 +1,96 @@
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+import tempfile
+import Image
+import numpy as np
+import sys
+import numm
+import threading
+
+from player import Player
+import graph
+
+
+class QPlayer(QtGui.QGraphicsPixmapItem):
+    def __init__(self, player):
+        self.player = player
+        QtGui.QGraphicsPixmapItem.__init__(self)
+        self.update()
+
+    def update(self):
+        # XXX: avoid passing through disk/png!
+        t = tempfile.NamedTemporaryFile(suffix='.png')
+        Image.fromarray(self.player.draw()).save(t.name)
+        self.setPixmap(QtGui.QPixmap(t.name))
+        t.close()
+
+    def mousePressEvent(self, event):
+        print 'press'
+        pos = event.scenePos()
+        nearest_node = g.nearest(pos.x(), pos.y())
+        p.trigger(nearest_node, 1.0)
+
+class QView(QtGui.QGraphicsView):
+    def __init__(self, player):
+        self.qplay = QPlayer(p)
+        scene = QtGui.QGraphicsScene()
+        scene.addItem(self.qplay)
+
+        QtGui.QGraphicsView.__init__(self, scene)
+
+        self.startTimer(100)
+
+    def keyPressEvent(self, event):
+        print event.text()
+        print event.key()
+
+        if event.key() == QtCore.Qt.Key_P:
+            p._target -= 1
+        if event.key() == QtCore.Qt.Key_N:
+            p._target += 1
+        if event.key() == QtCore.Qt.Key_D:
+            p._decay *= 1.1
+        if event.key() == QtCore.Qt.Key_E:
+            p._decay *= 0.9
+        if event.key() == QtCore.Qt.Key_S:
+            p._speed *= 1.1
+        if event.key() == QtCore.Qt.Key_W:
+            p._speed *= 0.9
+
+
+
+    def timerEvent(self, ev):
+        self.qplay.update()
+        QtGui.QGraphicsView.timerEvent(self, ev)
+
+if __name__=='__main__':
+    app = QtGui.QApplication(sys.argv)
+    app.setApplicationName('qplayer')
+
+    g = graph.load_graph(sys.argv[1]=='left')
+    graph.connect_to_samples(g, sys.argv[2:])
+
+    p = Player(g)
+
+    view = QView(p)
+
+    view.show()
+
+    def audio():
+        divisor = [1]
+        def a_out(a):
+            divisor[0] = len(p._state_nodes)
+            if divisor[0] == 0:
+                divisor[0] = 1
+
+            out = p.next(len(a))
+            divisor[0] = max(out.max() / float(2**15-1), divisor[0])
+
+            out /= divisor
+            a[:] = out.astype(np.int16)
+
+        numm.run(audio_out=a_out)
+    audiothread = threading.Thread(target=audio)
+    audiothread.start()
+
+    app.exec_()
