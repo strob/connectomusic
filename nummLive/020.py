6aebd8d115e881acd76564228d8efa88077c724c
Robert M Ochshorn <mail@RMOZONE.COM>
2012-11-21 10:25:09 +0100

    release sounds on inhibit

--- nummLive/020.py
+++ nummLive/020.py
@@ -1,4 +1,6 @@
 def inhibit():
     p._state_nodes = []
     p._state_edges = []
+    for n in p.graph.get_all_nodes():
+        n.release_sound()
 button("inhibit", inhibit)
--- nummLive/031.py
+++
@@ -1,3 +0,0 @@
-def selectNumber(idx):
-    p.select_by_nedges(idx, p._target)
-add_spin("select #", selectNumber, 1, 20)
